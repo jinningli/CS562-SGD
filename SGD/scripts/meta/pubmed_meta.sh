8f8b4e9b2f1504bfe3b80b0bc2a77190f846b0c2
jinningli <lijinning@sjtu.edu.cn>
2022-10-23 19:49:57 -0700

    first

---
+++ SGD/scripts/meta/pubmed_meta.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python train.py \
+    --debug \
+    --seed 10 \
+    --dataset pubmed \
+    --attack meta \
+    --ptb_rate 0.05 \
+    --alpha  0.3 \
+    --beta 2.5  \
+    --gamma 1 \
+    --lambda_ 0.001 \
+    --lr 1e-2 \
+    --epoch 100 \
+    --inner_steps 30
+    \
+     
